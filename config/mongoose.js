a31c723003a2d8edf830995eb07c1063a07e478b
ANKUSH PATIL <patil.ankush28@gmail.com>
2023-04-30 13:27:43 +0530

    Configured app code to cloud server for MongoDB

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,14 +1,25 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1/placement_dev");
-
+//mongoose.connect("mongodb://127.0.0.1/placement_dev");
+const url = `mongodb+srv://ckent8040:Google%40123@cluster0.0eq8is5.mongodb.net/?retryWrites=true&w=majority`;
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
 const db = mongoose.connection;
+mongoose.connect(url, connectionParams)
+    .then(() => {
+        console.log('Connected to the database ')
+    })
+    .catch((err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
 
-db.on("error", function (error) {
-    console.log("Error in connecting to the mongoose database ", error);
-});
+// db.on("error", function (error) {
+//     console.log("Error in connecting to the mongoose database ", error);
+// });
 
-db.once("open", function () {
-    console.log("Connected to the database :: MongoDB");
-})
+// db.once("open", function () {
+//     console.log("Connected to the database :: MongoDB");
+// })
 
 module.exports = db;
